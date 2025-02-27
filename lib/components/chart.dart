9e3c08bb2e99beac643895e658bc8bc462d2dd39
amazongas <amazongas@amazongas.com.br>
2025-02-27 09:49:13 -0400

    feature:creating-chart-data-dinamicly

--- lib/components/chart.dart
+++ lib/components/chart.dart
@@ -1,11 +1,29 @@
+import 'package:expenses/model/transaction.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class Chart extends StatelessWidget {
-  const Chart({super.key});
+  const Chart({required this.recentTransaction, super.key});
+
+  final List<Transaction> recentTransaction;
 
   List<Map<String, Object>> get groupedTransactions {
     return List.generate(7, (index) {
-      return {"day": "T", "value": 9.99};
+      final weekDay = DateTime.now().subtract(Duration(days: index));
+
+      double totalSum = 0.0;
+
+      for (var i = 0; i < recentTransaction.length; i++) {
+        bool dayMatch = recentTransaction[i].date.day == weekDay.day;
+        bool monthMatch = recentTransaction[i].date.month == weekDay.month;
+        bool yearMatch = recentTransaction[i].date.year == weekDay.year;
+
+        if (dayMatch && monthMatch && yearMatch) {
+          totalSum += recentTransaction[i].value;
+        }
+      }
+
+      return {"day": DateFormat.E().format(weekDay)[0], "value": totalSum};
     });
   }
 
@@ -14,7 +32,12 @@ class Chart extends StatelessWidget {
     return Card(
       elevation: 6,
       margin: EdgeInsets.all(20),
-      child: Row(children: const []),
+      child: Row(
+        children:
+            groupedTransactions.map((tr) {
+              return Text("${tr["day"]}: ${tr["value"]} | ");
+            }).toList(),
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'dart:math';
 
+import 'package:expenses/components/chart.dart';
 import 'package:expenses/components/transaction_form.dart';
 import 'package:expenses/components/transaction_list.dart';
 import 'package:expenses/model/transaction.dart';
@@ -24,7 +25,7 @@ class ExpensesApp extends StatelessWidget {
             fontFamily: "OpenSans",
             fontSize: 20,
             fontWeight: FontWeight.bold,
-            )
+          ),
         ),
         colorScheme: ColorScheme.fromSeed(
           seedColor: Colors.amber,
@@ -36,7 +37,8 @@ class ExpensesApp extends StatelessWidget {
           titleLarge: TextStyle(
             fontFamily: "OpenSans",
             fontSize: 18,
-            fontWeight: FontWeight.bold,)
+            fontWeight: FontWeight.bold,
+          ),
         ),
         useMaterial3: false,
       ),
@@ -53,19 +55,33 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   final List<Transaction> _transactions = [
-    // Transaction(
-    //   id: "t1",
-    //   title: "Novo tênis de corrida",
-    //   value: 310.76,
-    //   date: DateTime.now(),
-    // ),
-    // Transaction(
-    //   id: "t2",
-    //   title: "Conta de luz",
-    //   value: 211.30,
-    //   date: DateTime.now(),
-    // ),
+    Transaction(
+      id: "t0",
+      title: "Conta antiga",
+      value: 400.00,
+      date: DateTime.now().subtract(Duration(days: 33)),
+    ),
+    Transaction(
+      id: "t1",
+      title: "Novo tênis de corrida",
+      value: 310.76,
+      date: DateTime.now().subtract(Duration(days: 3)),
+    ),
+    Transaction(
+      id: "t2",
+      title: "Conta de luz",
+      value: 211.30,
+      date: DateTime.now().subtract(Duration(days: 4)),
+    ),
   ];
+
+  List<Transaction> get _recentTransactions {
+    return _transactions.where((tr) {
+      return tr.date.isAfter(DateTime.now().subtract(
+        Duration(days: 7),
+      ));
+    }).toList();
+  }
 
   void _openTransactionFormModal(BuildContext context) {
     showModalBottomSheet(
@@ -107,13 +123,7 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            Container(
-              child: Card(
-                elevation: 5,
-                color: Colors.blue,
-                child: Text('Gráfico'),
-              ),
-            ),
+            Chart(recentTransaction: _recentTransactions),
             TransactionList(transactions: _transactions),
           ],
         ),
