332e63bcc45ea42175b0aca7200a71b0f05c5d05
amazongas <amazongas@amazongas.com.br>
2025-02-27 17:26:24 -0400

    chore:daily-update-but-not-finished

--- lib/components/chart_bar.dart
+++ lib/components/chart_bar.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 
 class ChartBar extends StatelessWidget {
-  const ChartBar({super.key});
+  const ChartBar({required this.label, this.value = 0.0, this.percentage = 0.0, super.key});
+
+  final String label;
+  final double value;
+  final double percentage;
 
   @override
   Widget build(BuildContext context) {
